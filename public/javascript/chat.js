9e2843a528da8e7f3ad56b7840054e83a4556a4b
Patryk Laba <laba.patryk@gmail.com>
2018-06-04 21:05:46 +0200

    chat commands processing logic added

---
+++ public/javascript/chat.js
@@ -0,0 +1,42 @@
+const Chat = function(socket) {
+  this.socket = socket;
+}
+
+Chat.prototype.sendMessage = function(room, text) {
+  this.socket.emit('message', {
+    room: room,
+    text: text
+  });
+}
+
+Chat.prototype.changeRoom = function(room) {
+  this.socket.emit('join', {
+    newRoom: room
+  });
+};
+
+Chat.prototype.changeName = function(newName) {
+  this.socket.emit('client:ChangeNick', newName);
+}
+
+Chat.prototype.processCmd = function(cmd) {
+  const words = cmd.split(' ');
+  const command = words[0].slice(1).toLowerCase();
+  let message = false;
+  switch(command) {
+    case 'join':
+      words.shift();
+      const room = words.join(' ');
+      this.changeRoom(room);
+      break;
+    case 'nick':
+      words.shift();
+      const name = words.join(' ');
+      this.changeName(name);
+      break;
+    default: 
+      message: 'Unknown command';
+      break;
+  }
+  return message;
+};
