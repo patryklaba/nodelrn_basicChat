d8082cea771121c0be8f5adbf79ed2859aaf0ea9
Patryk Laba <laba.patryk@gmail.com>
2018-06-05 20:56:50 +0200

    added styles and logic for various types of messages

--- public/javascript/chat_ui.js
+++ public/javascript/chat_ui.js
@@ -1,38 +1,55 @@
 const socket = io();
-
+const ui = {
+  messages: document.querySelector('#messages'),
+  form: document.querySelector('#send-form'),
+  messageInput: document.querySelector('#send-message'),
+  sendBtn: document.querySelector('#send-button')
+};
 
 function sanitize(text) {
   return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'); 
 }
 
-function createMessageEl(message) {
+function createMessageEl(message, type) {
   let el = document.createElement('div');
-  el.innerText = sanitize(message.text);
+  if(type) el.className = `${type}-message`;
+  el.innerText = (!type) ? sanitize(message.text) : sanitize(`Me: ${message.text}`);
   return el;
+}
+
+function appendMessage(message, type) {
+  const msgElement = createMessageEl(message, type);
+  ui.messages.appendChild(msgElement);
+  ui.messageInput.value = '';
+  ui.messages.scrollTop = ui.messages.scrollHeight;
+}
+
+function info(text) {
+  appendMessage({text}, 'info');
 }
 
 
 
 $(document).ready( () => {
   const chatApp = new Chat(socket);
-  const ui = {
-    messages: document.querySelector('#messages'),
-    form: document.querySelector('#send-form'),
-    messageInput: document.querySelector('#send-message'),
-    sendBtn: document.querySelector('#send-button')
-  };
+  
 
   socket.on('message', (message) => {
-    ui.messages.appendChild(createMessageEl(message));
-    ui.messageInput.value = '';
+    appendMessage(message);
   })
 
   ui.form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const message = {};
-    message["text"] = ui.messageInput.value;
-    message["room"] = "Lobby";
-    socket.emit('message', message);
+    const textValue = ui.messageInput.value;
+    if(textValue.length > 1) {
+      const message = {};
+      message["text"] = textValue;
+      message["room"] = "Lobby";
+      socket.emit('message', message);
+      appendMessage(message, 'user');
+    } else {
+      info('your message should have at least 2 characters', 'info');
+    }
     return false;
   })
 });
